b56e22a27d678a915c23456cc70a96f4954ad8b1
Aditya Gupta <109871314+Nightkilller@users.noreply.github.com>
2026-08-13 19:12:16 +0530

    Implement stack-based character removal algorithm

---
+++ 21.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        string s;
+
+        cin >> n >> s;
+
+        stack<char> st;
+
+        for (char c : s) {
+            if (!st.empty() && st.top() == c) {
+                st.pop();
+            } else {
+                st.push(c);
+            }
+        }
+
+        cout << (st.empty() ? "YES" : "NO") << '\n';
+    }
+
+    return 0;
+}
